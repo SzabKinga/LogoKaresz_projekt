8129da523070cd7b5f9f42584d805d02d18a23d7
Kinga Szabó <szabo.kinga.21f@szlgbp.hu>
2022-03-03 10:58:11 +0100

    proba

---
+++ Kinga.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LogoKaresz
+{
+    public partial class Form1
+    {
+        void koszon()
+        {
+            MessageBox.Show("Hello!");
+        }
+    }
+}
