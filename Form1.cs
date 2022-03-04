5a181ead65496ba1ab4e02a5b4a7312da144cebe
Emese Török <torok.emese.21f@szlgbp.hu>
2022-03-04 16:26:07 +0100

    Külső vonal bal kívül

--- Form1.cs
+++ Form1.cs
@@ -7,13 +7,55 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
-		/* Függvények */
+		void Külső_vonal_bal(double meret)
+        {
 
 
-		/* Függvények vége */
+			Balra(90);
+            Bezier(50, 0, 50, 80, 100, false, false);
+			Jobbra(50);
+			for (int i = 0; i < 10; i++)
+			{
+				Előre(1);
+				Balra(1 + i);
+
+			}
+
+			Tollat(fel);
+			Jobbra(5);
+			Hátra(25);
+			Jobbra(90);
+			Hátra(105);
+
+			Tollat(le);
+			
+
+
+
+
+
+
+
+
+
+
+		}
+
+		void Külső_vonal_jobb(double meret)
+		{
+
+
+			Bezier(-100, 175, -50, 120, 150, false, false);
+
+
+
+		}
 		void FELADAT()
 		{
-			MessageBox.Show("O.O");
+			Külső_vonal_bal(100);
+
+
+
 		}
 	}
 }
